13d2fcf53435d9d8458f21bcbd8508ba18ac73ba
wildboy2arthur <wildboy.arthur@gmail.com>
2024-05-18 07:17:50 +0000

    Added a new print statement

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 def hello_world():
     print("Hello, World!")
     print("This is a simple Python project.")
+    print("Now it has been modified!")
 
 if __name__ == "__main__":
     hello_world()
